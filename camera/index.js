3d7b1010a53db105271a14302d7cb84fb7ecebf6
MatthieuLemoine <ml.94230@gmail.com>
2016-12-19 22:32:53 +0100

    :heavy_plus_sign: uws for camera module

--- camera/index.js
+++ camera/index.js
@@ -1,9 +1,8 @@
 const http            = require('http');
 const express         = require('express');
 const path            = require('path');
-const util            = require('util');
 const spawn           = require('child_process').spawn;
-const WebSocketServer = require('ws').Server;
+const WebSocketServer = require('uws').Server;
 const Splitter        = require('stream-split');
 
 const PORT         = process.env.PORT || 3000;
@@ -12,7 +11,6 @@ const NALseparator = new Buffer([0, 0, 0, 1]);
 // Express app
 const app  = express();
 app.use(express.static(path.join(__dirname, 'public')));
-
 const server  = http.createServer(app);
 
 const options = {
@@ -24,33 +22,36 @@ const options = {
 const wss = new WebSocketServer({ server });
 wss.on('connection', onNewClient);
 let readStream;
+let child;
+
+// WS streaming
 
 function onNewClient(socket) {
-  console.info('New client');
-
   socket.send(JSON.stringify({
     action : 'init',
     width  : options.width,
     height : options.height,
   }));
-
   socket.on('message', data => {
     const cmd = `${data}`;
     const action = cmd.split(' ')[0];
-    console.info("Incomming action '%s'", action);
-
     if (action === 'REQUESTSTREAM') {
       startFeed();
     } else if (action === 'STOPSTREAM' && readStream) {
-      readStream.pause();
-    }
-  });
-
-  socket.on('close', () => {
-    if (readStream) {
       readStream.end();
+      child.stdout.pause();
     }
   });
+  socket.on('close', killChild);
+}
+
+function killChild() {
+  if (child) {
+    child.kill('SIGINT');
+    readStream.end();
+  }
+  readStream = null;
+  child      = null;
 }
 
 function startFeed() {
@@ -65,26 +66,29 @@ function broadcast(data) {
       return;
     }
     socket.buzy = false;
-    socket.send(Buffer.concat([NALseparator, data]), { binary : true }, error => {
+    socket.send(Buffer.concat([NALseparator, data]), { binary : true }, () => {
       socket.buzy = false;
-      console.error(error);
     });
   });
 }
 
 function getFeed() {
-  const msk = 'raspivid -t 0 -o - -w %d -h %d -fps %d';
-  const cmd = util.format(msk, options.width, options.height, options.fps);
-  console.info(cmd);
-  const streamer = spawn(
-    'raspivid',
-    [
-      '-t', '0', '-o', '-', '-w', options.width,
-      '-h', options.height, '-fps', options.fps,
-      '-pf', 'baseline'
-    ]);
-  streamer.on('exit', code => console.log('Failure', code));
-  return streamer.stdout;
+  if (child) {
+    child.stdout.resume();
+  } else {
+    child = spawn(
+      'raspivid',
+      [
+        '-t', '0', '-o', '-', '-w', options.width,
+        '-h', options.height, '-fps', options.fps,
+        '-pf', 'baseline'
+      ]);
+    child.on('exit', code => console.log('Camera process exiting', code));
+  }
+  return child.stdout;
 }
 
 server.listen(3000, () => console.info(`Video surveillance service started on port ${PORT}`));
+
+// Kill child on crash
+process.on('uncaughtException', killChild);
