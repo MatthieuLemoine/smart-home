771eff96272e78e98a457e6c36f30ddcb4e671d5
MatthieuLemoine <ml.94230@gmail.com>
2016-12-22 00:00:12 +0100

    :sparkles: Camera remote server

--- camera/remote-server/index.js
+++ camera/remote-server/index.js
@@ -1,1 +1,19 @@
-// TODO implements live remote backup of videos
+const express = require('express');
+const fs      = require('fs');
+const path    = require('path');
+const logger  = require('../logger');
+const dateFns = require('date-fns');
+
+const app = express();
+
+const PORT      = process.env.PORT || 3001;
+
+app.post('/feed', (req) => {
+  const videoPath = path.join(
+    __dirname, 'videos', `${dateFns.format(new Date(), 'YYYY-MM-DD HH:mm:ss')}.h264`
+  );
+  const fileStream = fs.createWriteStream(videoPath);
+  req.pipe(fileStream);
+});
+
+app.listen(3000, () => logger.info(`Remote video backup service started on port ${PORT}`));
