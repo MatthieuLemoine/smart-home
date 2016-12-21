fee8cd2433d85b2c28bf44ef5a77f475b24920b8
MatthieuLemoine <ml.94230@gmail.com>
2016-12-21 23:29:35 +0100

    :bug: Fix missing pipe after video clean

--- camera/server/index.js
+++ camera/server/index.js
@@ -1,15 +1,16 @@
-const http            = require('http');
-const express         = require('express');
-const path            = require('path');
-const initWS          = require('./ws');
-const record          = require('./record');
+const http    = require('http');
+const express = require('express');
+const path    = require('path');
+const logger  = require('../logger');
+const initWS  = require('./ws');
+const record  = require('./record');
 
 const PORT = process.env.PORT || 3000;
 
 // Express app
-const app  = express();
+const app    = express();
 app.use(express.static(path.join(__dirname, '..', 'dist')));
-const server  = http.createServer(app);
+const server = http.createServer(app);
 
 const options = {
   width  : 640,
@@ -22,7 +23,7 @@ record.start();
 
 initWS(server, options, record);
 
-server.listen(3000, () => console.info(`Video surveillance service started on port ${PORT}`));
+server.listen(3000, () => logger.info(`Video surveillance service started on port ${PORT}`));
 
 // Kill child on crash
 process.on('uncaughtException', record.stop);
--- camera/server/record.js
+++ camera/server/record.js
@@ -1,10 +1,12 @@
-const spawn = require('child_process').spawn;
-const fs    = require('fs');
-const path  = require('path');
+const spawn  = require('child_process').spawn;
+const fs     = require('fs');
+const path   = require('path');
+const logger = require('../logger');
 
 let recordProcess;
 let options;
-const videoFile      = path.join(__dirname, '..', 'videos', 'out.h264');
+const videoFile       = path.join(__dirname, '..', 'videos', 'out.h264');
+const backupVideoFile = path.join(__dirname, '..', 'videos', 'back.h264');
 let localVideoStream = fs.createWriteStream(videoFile);
 
 // Clear output video every 2 hours
@@ -48,10 +50,17 @@ function setOptions(opts) {
 }
 
 function clearOutput() {
+  logger.info('Clear output video');
   if (recordProcess) {
     recordProcess.stdout.unpipe(localVideoStream);
   }
   localVideoStream.end();
-  // w flag by default i.e replace file
-  localVideoStream = fs.createWriteStream(videoFile);
+  // Backup video
+  fs.rename(videoFile, backupVideoFile, () => {
+    // w flag by default i.e replace file
+    localVideoStream = fs.createWriteStream(videoFile);
+    if (recordProcess) {
+      recordProcess.stdout.pipe(localVideoStream);
+    }
+  });
 }
